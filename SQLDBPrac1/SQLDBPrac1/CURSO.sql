be2f54e1d6ba71ccebf1c52953420852f1ad1bb9
Rony <rramirez3@udi.edu.co>
2021-09-02 19:54:31 -0500

    Crear Tablas

--- SQLDBPrac1/SQLDBPrac1/CURSO.sql
+++ SQLDBPrac1/SQLDBPrac1/CURSO.sql
@@ -2,7 +2,7 @@
 (
 	[Codigo] INT NOT NULL PRIMARY KEY, 
     [ Nombre] NVARCHAR(50) NOT NULL, 
-    [Fecha_Inicio] NCHAR(10) NOT NULL, 
+    [Fecha_Inicio] NVARCHAR(50) NOT NULL, 
     [Duracion] INT NOT NULL, 
     [Valor] INT NOT NULL
 )
